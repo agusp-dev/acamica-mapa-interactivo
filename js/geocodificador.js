f88843830e1e5c814db92c47b23cf0c486314c3e
Perez Agustin <agustn.jorgex.prez.pereyra@intel.com>
2019-05-22 18:27:50 -0300

    Se crea objeto mapa y se visualiza.

--- js/geocodificador.js
+++ js/geocodificador.js
@@ -1,3 +1,6 @@
+var xhhtp = new XMLHttpRequest();
+var url;
+
 geocodificadorModulo = (function () {
   var geocodificador // Geocodificador que dada una dirección devuelve una coordenada
   
@@ -8,6 +11,24 @@ geocodificadorModulo = (function () {
      y que llame a la función pasada por parámetro con los siguientes parámetros
      dirección: la dirección pasada por parámetro
      coordenada: la ubicación de tipo google.maps.LatLng */
+
+     console.log("Direccion: " + direccion);
+
+     geocodificador.geocode({'address': direccion}, function(results, status) {
+       console.log('status: ' + status);
+       console.log('results: ' + results[0].geometry.location);
+       console.log('location: ' + results[0].geometry.location[0]);
+
+      if (status === 'OK') {
+        funcionALlamar(direccion, getLatLng(results[0].geometry.location));
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  function getLatLng(location) {
+    return new google.maps.LatLng({});
   }
 
     // Inicializo el geocoder que obtiene las corrdenadas a partir de una dirección
@@ -33,3 +54,16 @@ geocodificadorModulo = (function () {
     inicializar
   }
 })()
+
+var getUrl = function() {
+  return 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway+Mountain+View';
+}
+
+var apiConnect = function(xhhtp, url) {
+  xhhtp.open('GET', url, true);
+  xhhtp.send();
+}
+
+xhhtp.onreadystatechange = function() {
+  console.log(this.readyState + " - " + this.status);
+}
